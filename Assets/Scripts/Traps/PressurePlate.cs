42b13816d7eee6f83edcb0729e739aad2f1858ee
Nikita Tulin <ntulin.work@gmail.com>
2024-09-26 22:09:14 -0300

    Add pressable states for pressure plate

--- Assets/Scripts/Traps/PressurePlate.cs
+++ Assets/Scripts/Traps/PressurePlate.cs
@@ -1,21 +1,101 @@
+using System.Collections;
 using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    #region Serialized Fields
+
     [SerializeField]
     private SimpleTrap trap;
 
+    [SerializeField]
+    private LayerMask activatedBy;
+
+    [SerializeField]
+    private Transform visuals;
+
+    [SerializeField]
+    private Vector3 visualsPressedPosition;
+
+    [SerializeField]
+    private float activateTime = 0.5f;
+
+    #endregion
+
+    private bool activated;
+
+    private int collisionsCount;
+    private Vector3 originalPosition;
+
+    #region Event Functions
+
+    private void Awake()
+    {
+        originalPosition = visuals.localPosition;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<IEntityHealth>() != null)
+        if ((activatedBy.value & (1 << other.gameObject.layer)) == 0)
         {
-            Debug.Log("Pressure detected");
-            Interact();
+            return;
+        }
+
+        collisionsCount++;
+
+        if (collisionsCount == 1 && !activated)
+        {
+            StartCoroutine(Activate());
         }
     }
 
-    public void Interact()
+    private void OnTriggerExit(Collider other)
     {
+        if ((activatedBy.value & (1 << other.gameObject.layer)) == 0)
+        {
+            return;
+        }
+
+        collisionsCount--;
+
+        if (collisionsCount == 0)
+        {
+            StartCoroutine(Deactivate());
+        }
+    }
+
+    #endregion
+
+    private IEnumerator Activate()
+    {
+        activated = true;
+
+        var elapsedTime = 0f;
+        while (elapsedTime < activateTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            visuals.localPosition =
+                Vector3.Lerp(visuals.localPosition, visualsPressedPosition, elapsedTime / activateTime);
+
+            yield return null;
+        }
+
         trap.Shoot();
+    }
+
+    private IEnumerator Deactivate()
+    {
+        var elapsedTime = 0f;
+        while (elapsedTime < activateTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            visuals.localPosition = Vector3.Lerp(visuals.localPosition, originalPosition, elapsedTime / activateTime);
+
+            yield return null;
+        }
+
+        activated = false;
     }
 }
